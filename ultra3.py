d4d28ab696ca72963ddd7a99cfe25a5ef85f2f2c
ihabmalaouiH <157219436+ihabmalaouiH@users.noreply.github.com>
2025-12-17 12:47:35 +0100

    Update ultra3.py

--- ultra3.py
+++ ultra3.py
@@ -261,7 +261,7 @@ def update_firestore_db(matches_list):
     try:
         # استخدام Batch لضمان السرعة في التحديث
         batch = db.batch()
-        collection_ref = db.collection('matches')
+        collection_ref = db.collection('today')
 
         count = 0
         for match in matches_list:
